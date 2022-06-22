5dfa46275b2c739cd4540e84146f32751a9a639a
Alexandre280 <alexandresouza280@gmail.com>
2022-06-21 22:57:35 -0300

    commit 21/06
    Exercício 1007

---
+++ 1007.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+print('DIFERENCA = {}'.format(A*B-C*D))
+
+
+# In[ ]:
+
+
+5
+
