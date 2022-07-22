7c9b89726e2b4d1997decf3018c52fb8db1f94a1
victornico15 <vnespindola15@gmail.com>
2022-07-21 22:20:08 -0300

    Commit 21/07 new problems

---
+++ Problem Level 1/1052.py
@@ -0,0 +1,27 @@
+VarInput=int(input())
+
+
+if(VarInput==1):
+    print("January")
+elif(VarInput==2):
+    print("February")
+elif(VarInput==3):
+    print("March")
+elif(VarInput==4):
+    print("April")
+elif(VarInput==5):
+    print("May")
+elif(VarInput==6):
+    print("June")
+elif(VarInput==7):
+    print("July")
+elif(VarInput==8):
+    print("August")
+elif(VarInput==9):
+    print("September")
+elif(VarInput==10):
+    print("October")
+elif(VarInput==11):
+    print("November")
+elif(VarInput==12):
+    print("December")
