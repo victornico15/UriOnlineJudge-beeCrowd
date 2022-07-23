2c0b78db5eedbbe68e4c91611e5cc763a2b08676
victornico15 <vnespindola15@gmail.com>
2022-07-23 11:21:36 -0300

    melhorando e organizando.

--- Funções prontas.py
+++ Funções prontas.py
@@ -1,10 +1,4 @@
-
-frutas = ["maçã", "banana", "laranja", "melancia"]
-
-lista = [fruta for fruta in frutas]
-print(lista)
-
-#  METODO DE RECEBER LISTA  QND CRIADA DENTRO DE UM LACO EM UMA UNICA LINHA
+# CONVERSORES 
 
 # Função de Conversao em Int
 ###CONVERSOR MELHOR   a,c,b,d=list(map(int,input().split()))
@@ -14,5 +8,21 @@ def conversorInt(A,B,C):
     B=int(B)
     C=int(C)
     return(A,B,C)
+
+
+
+
+# manipulacao de lista 
+
+#multiplicando a lista
+inteiros = [1,3,4,5,7,8]
+quadrados = [n*n for n in inteiros]
+frutas = ["maçã", "banana", "laranja", "melancia"]
+
+lista = [fruta for fruta in frutas]
+print(lista)
+
+#  METODO DE RECEBER LISTA  QND CRIADA DENTRO DE UM LACO EM UMA UNICA LINHA
+
 
 
--- Rascunho de codigos.py
+++ Rascunho de codigos.py
@@ -1,6 +1,14 @@
+
+
+# manipulacao de lista 
+
+#multiplicando a lista
+inteiros = [1,3,4,5,7,8]
+quadrados = [n*n for n in inteiros]
 frutas = ["maçã", "banana", "laranja", "melancia"]
 
 lista = [fruta for fruta in frutas]
 print(lista)
 
 #  METODO DE RECEBER LISTA  QND CRIADA DENTRO DE UM LACO EM UMA UNICA LINHA
+
