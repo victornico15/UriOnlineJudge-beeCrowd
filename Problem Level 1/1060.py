388cf227d0390d48005a49329c83e42a02aa0707
victornico15 <vnespindola15@gmail.com>
2022-07-23 11:01:28 -0300

    Reform

--- Problem Level 1/1060.py
+++ Problem Level 1/1060.py
@@ -1,11 +1,16 @@
-lista1=list(map(float,input().split()))
+"""lista1=list(map(float,input().split()))
 count=0
 for i in range(len(lista1)):
     if(lista1[i]>=0):
         count+=1
 
-print(count,"valores positivos")
+print(count,"valores positivos")"""
 
+###################
 
-
-    
+y = 0
+for x in range(1,7):
+    a = float(input())
+    if a > 0:
+        y = y + 1
+print('{} valores positivos'.format(y))
