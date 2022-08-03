34f1dfbc9c347ac186f46d0d478e164c93e0d388
victornico15 <vnespindola15@gmail.com>
2022-08-03 18:13:06 -0300

    Commit-1075 - Resto 2

---
+++ 1075.py
@@ -0,0 +1,11 @@
+"""VarInput=int(input())
+
+for i in range (1001):
+    if(i%VarInput==2):
+        print(i)
+        """
+
+n=int(input())
+for i in range(1,10000):
+    if(i%n==2):
+        print(i)
