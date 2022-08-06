cdb86082ec33b47d8d278ee9f1c2589c81823718
victornico15 <vnespindola15@gmail.com>
2022-08-05 21:03:26 -0300

    Media ponderada em lista

---
+++ Problem Level 1/1079.py
@@ -0,0 +1,8 @@
+VarInput=int(input())
+
+for i in range(VarInput):    
+    lista1=list(map(float,input().split()))  
+    print("{:.1f}".format( (lista1[0]*2 +lista1[1]*3 + lista1[2]*5)/10))     
+
+#brabo, orgulho aqui 
+    
