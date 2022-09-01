0cd75fef0395d62c7ce9786d0f4edb0f949653ea
victornico15 <vnespindola15@gmail.com>
2022-09-01 17:10:49 -0300

    Sequências Crescentes

---
+++ Problem Level 1/1146.py
@@ -0,0 +1,29 @@
+"""
+
+
+while(True):
+    
+    X=int(input())
+    if(X==0):
+        break
+    for i in range (1,X):
+        print(i,"",end="")        
+        if(i==X-1):
+         print(i+1)
+       
+    """
+    
+    
+    i = True
+j=1
+while i:
+    X = int(input())
+    if X == 0:
+        break
+    j = 1
+    while j <= X:
+        if j==X:
+            print('%d'%(j),end="\n")
+        else:
+            print('%d'%(j),end=" ")
+        j+=1
